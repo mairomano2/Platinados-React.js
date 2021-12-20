80a61d224c0c97cbe6523229001b34fb2230782d
Maia Romano <mairomano2@gmail.com>
2021-12-19 23:37:07 -0300

    retoques finales de estilos y agregado de imagenes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import Imagen from "../Imagenes/sega.png"
 import "./landing.css"
 
 export function Landing(){
   return(
     <div className="hero">
-      {/* <img className="hero-img" src={sega} /> */}
-      <h1 className="titulo">Bienvenidx a platinados</h1>
+      <img className="hero-img" src={Imagen} />
+      <h1 className="titulo" id="h1">Bienvenidx a platinados</h1>
       <h2 className="subtitulos">Comenza a mejorar tu vida</h2>
       <div  className="botones">
         <Link to="/misiones">
